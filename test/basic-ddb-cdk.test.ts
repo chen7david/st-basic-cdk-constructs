5e3df27e10ceb0b4e781d45796a3e86e2f506422
David M. Chen <chen7david@me.com>
2023-06-04 09:13:48 -0300

    Initial commit

---
+++ test/basic-ddb-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BasicDdbCdk from '../lib/basic-ddb-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/basic-ddb-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BasicDdbCdk.BasicDdbCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
